82601a9297f8ee3cf6758f946591cca093411c6a
aaaa-zhen <43779067+aaaa-zhen@users.noreply.github.com>
2025-06-04 10:05:55 +0800

    Add portfolio website template

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    document.querySelectorAll('.fade').forEach(section => {
+        observer.observe(section);
+    });
+});
